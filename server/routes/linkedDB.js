17a49bf6a131b97bfa5371b42c86469a0cec6c6a
hezhang18 <hezhang18@outlook.com>
2020-08-08 22:49:10 +0800

    Update

--- server/routes/linkedDB.js
+++ server/routes/linkedDB.js
@@ -3,10 +3,8 @@ let state = '';
 
 let linkedDB = (()=>{
 	// 连接本地数据库
-	// mongoose.connect('mongodb://Jeff:Jeff1994%40Shanghai@127.0.0.1:27017/vStarbucks', {useNewUrlParser: true});
-	//连接服务器数据库
-	mongoose.connect('mongodb://Jeff:Jeff1994%40Shanghai@47.114.181.210:27017/vSbux', {useNewUrlParser: true});
-
+	mongoose.connect('mongodb://UserName:PassWord@127.0.0.1:27017/vSbux', {useNewUrlParser: true});
+	
 	mongoose.connection.on("connected", ()=>{
 		console.log("MongoDB Connected Success!");
 	});
